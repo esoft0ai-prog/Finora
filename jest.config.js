a910c270db1720a02253ebd79ca428a4378fb609
esoft0ai-prog <esoft0ai@gmail.com>
2026-09-25 18:14:37 +0100

    test: simplify Jest module mocks

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: '@react-native/jest-preset',
-  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   transformIgnorePatterns: ['node_modules/(?!(jest-)?react-native|@react-native|@notifee)'],
 };
